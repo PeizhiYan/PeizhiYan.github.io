1079143d1a0cf8edc2a978e29a944f7b4b9987cf
Peizhi Yan (Matthew) <34731246+PeizhiYan@users.noreply.github.com>
2019-08-11 00:13:14 -0400

    CSS

--- www/script/draw.js
+++ www/script/draw.js
@@ -15,6 +15,9 @@ var canvas, context;
 let path_stack = []; // paths are saved in here
 let redo_stack = []; // removed paths are saved in here
 var view_center; // the origin of the view coordinate
+var current_color = "#FFFFFF";
+var opacity = "FF"; // from 0 to 255
+var frequent_color_pointer = 0; // 8 frequently used colors
 
 window.onload = function() {
     paper.setup('myCanvas');
@@ -26,25 +29,25 @@ window.onload = function() {
     view.center = view_center; // do not remove this line !!!
     view_zoom = view.zoom; // get the view zoom factor
     view.zoom = view_zoom; // do not remove this line !!!
-    current_color = "black";
-
-
-
-
+    current_color = "#FFFFFF";
 
     ///////////////////////////////////////////////////////////////////
     ////// support mobile device's touch screen: Zoom and/or Scroll
     draw_lock = false; // when it's true, not draw anything
     move_lock = false;
+    zoom_lock = false;
     var prev_distance = 0;
     var current_distance = 0;
     var prev_X = -1;
     var prev_Y = -1;
+    var prev_x0 = -1;
+    var prev_x1 = -1;
+    var prev_y0 = -1;
+    var prev_y1 = -1;
     function touch_screen(event) {
-        //print(path_stack.length)
         if (event.touches[1] != undefined && event.touches[2] != undefined) {
             draw_lock = true;
-            // Move with three fincers
+            // Move with three fingers
             if (prev_X == -1){
                 prev_X = event.touches[0].clientX;
                 prev_Y = event.touches[0].clientY;
@@ -56,21 +59,24 @@ window.onload = function() {
                 prev_X = event.touches[0].clientX;
                 prev_Y = event.touches[0].clientY;
             }
-            //print(prev_X+","+prev_Y)
         }
         else if (event.touches[1] != undefined) {
+            // Move with two fingers 
             draw_lock = true; // lock: cannot draw
-            //print("pinch start")
             var x1 = event.touches[1].clientX;
             if (prev_distance == 0){
                 prev_distance = distance(event.touches[0].clientX, event.touches[0].clientY, event.touches[1].clientX, event.touches[1].clientY)
                 prev_X = event.touches[0].clientX;
                 prev_Y = event.touches[0].clientY;
+                prev_x0 = event.touches[0].clientX;
+                prev_x1 = event.touches[1].clientX;
+                prev_y0 = event.touches[0].clientY;
+                prev_y1 = event.touches[1].clientY;
             }
             
             current_distance = distance(event.touches[0].clientX, event.touches[0].clientY, event.touches[1].clientX, event.touches[1].clientY)
             scale_ratio = (current_distance/prev_distance);
-            if (scale_ratio >= 0.9 && scale_ratio <= 1.2 && move_lock == false) {
+            if (scale_ratio >= 0.7 && scale_ratio <= 1.4 && move_lock == false) {
                 view_center.x -= (event.touches[0].clientX - prev_X)/view_zoom;
                 view_center.y -= (event.touches[0].clientY - prev_Y)/view_zoom;
                 view.center = view_center;
@@ -80,19 +86,30 @@ window.onload = function() {
             else {
                 move_lock = true;
                 // Zoom with two fingers
+                ////////////////print("prev distance: "+prev_distance)
                 current_distance = distance(event.touches[0].clientX, event.touches[0].clientY, event.touches[1].clientX, event.touches[1].clientY)
                 scale_ratio = (current_distance/prev_distance);
-                if (scale_ratio > 2){
-                    view_zoom += 0.07; // Zoom in
+
+                // log for any base
+                function log(base, n) {
+                    return Math.log10(n)/Math.log10(base);
                 }
+                // Zoom in
+                if (scale_ratio > 1.4) {
+                    zoom_rate = 0.2*(1-log(20,view.zoom));
+                    view_zoom += zoom_rate;
+                }
+                // Zoom out
                 else if (scale_ratio < 0.7){
-                    view_zoom -= 0.07; // Zoom out
+                    zoom_rate = 0.8*log(10,view.zoom)
+                    view_zoom -= zoom_rate;
                 }
-                if (view_zoom <= 0.9){
-                    view_zoom = 0.9; // minimum zoom factor
+
+                if (view_zoom <= 1){
+                    view_zoom = 1; // minimum zoom factor
                 }
-                else if (view_zoom >= 2.5){
-                    view_zoom = 2.5; // maximum zoom factor
+                else if (view_zoom >= 15.5){
+                    view_zoom = view.zoom; // maximum zoom factor
                 }
                 view.zoom = view_zoom;
                 //print(view_zoom)
@@ -108,9 +125,14 @@ window.onload = function() {
         }
         draw_lock = false; // unlock: can draw
         move_lock = false;
+        zoom_lock = false;
         prev_distance = 0; // reset the distance register
         prev_X = -1;
         prev_Y = -1;
+        prev_x0 = -1;
+        prev_x1 = -1;
+        prev_y0 = -1;
+        prev_y1 = -1;
         //print("touch stop")
     }
     canvas.addEventListener("touchend", touch_end);
@@ -163,7 +185,7 @@ window.onload = function() {
         //sleep(10);
         if (draw_lock == false && drag_lock == false){
             path = new Path();
-            path.strokeColor = current_color;
+            path.strokeColor = current_color+opacity;
             path.add(event.point);
         }
         if (drag_lock) {
@@ -245,10 +267,10 @@ window.onload = function() {
     tool4.onMouseDown = function onMouseDown(event) {
         if (draw_lock == false && drag_lock == false){
             path = new Path();
-            path.strokeColor = current_color;
+            path.strokeColor = current_color + opacity;
             path.strokeWidth = 0.5; 
             path.add(event.point);
-            path.fillColor = current_color;
+            path.fillColor = current_color + opacity;
         }
         if (drag_lock) {
             prev_X = event.point.x;
@@ -312,77 +334,62 @@ function reset_everything() {
 }
 
 // change path color
+var color_1 = "#17202A";
+var color_2 = "#E74C3C";
+var color_3 = "#7D3C98";
+var color_4 = "#2E86C1";
+var color_5 = "#229954";
+var color_6 = "#F39C12";
+var color_7 = "#A6ACAF";
+var color_8 = "#5D6D7E";
 function set_color(c_str) {
     current_color = c_str;
-    document.getElementById("current-color").style.backgroundColor = c_str;
-    if (document.getElementById("pen").style.color != "gray"){
-        document.getElementById("pen").style.color = c_str;
+    switch(c_str){
+        case "color_1": current_color = color_1; break;
+        case "color_2": current_color = color_2; break;
+        case "color_3": current_color = color_3; break;
+        case "color_4": current_color = color_4; break;
+        case "color_5": current_color = color_5; break;
+        case "color_6": current_color = color_6; break;
+        case "color_7": current_color = color_7; break;
+        case "color_8": current_color = color_8; break;
     }
-    else if (document.getElementById("curve-pen").style.color != "gray"){
-        document.getElementById("curve-pen").style.color = c_str;
-    } 
-    else if (document.getElementById("roll").style.color != "gray"){
-        document.getElementById("roll").style.color = c_str;
-    } 
+    document.getElementById("palette_icon").style.color = current_color;
+    document.getElementById("palette").style.backgroundColor = current_color;
+    document.getElementById("color_picker").setAttribute("value", current_color);
+    document.getElementById("color_picker").value = current_color;
+}
+
+function pick_color () {
+    var get_color = document.getElementById("color_picker").value;
+    set_color(get_color);
+    switch(frequent_color_pointer){
+        case 0: color_1 = get_color; document.getElementById("color_1").style.backgroundColor=get_color; break;
+        case 1: color_2 = get_color; document.getElementById("color_2").style.backgroundColor=get_color; break;
+        case 2: color_3 = get_color; document.getElementById("color_3").style.backgroundColor=get_color; break;
+        case 3: color_4 = get_color; document.getElementById("color_4").style.backgroundColor=get_color; break;
+        case 4: color_5 = get_color; document.getElementById("color_5").style.backgroundColor=get_color; break;
+        case 5: color_6 = get_color; document.getElementById("color_6").style.backgroundColor=get_color; break;
+        case 6: color_7 = get_color; document.getElementById("color_7").style.backgroundColor=get_color; break;
+        case 7: color_8 = get_color; document.getElementById("color_8").style.backgroundColor=get_color; break;
+    }
+    frequent_color_pointer += 1;
+    if (frequent_color_pointer >= 8){
+        frequent_color_pointer = 0;
+    }
+}
+
+function to_hex(decimal) {
+    ret = decimal.toString(16);
+    if (ret.length == 1){
+        return "0"+ret;
+    }
+    return ret;
+}
+
+function opacity_change () {
+    opacity = to_hex(parseInt(document.getElementById("opacity").value));
+    document.getElementById("palette").style.backgroundColor = current_color+opacity;
 }
 
 
-// move toolbar to the left side
-function toolbar_left() {
-    document.getElementById("color-container").style.left = "0px";
-    document.getElementById("tool-container").style.left = "0px";
-    document.getElementById("color-container").style.right = "";
-    document.getElementById("tool-container").style.right = "";
-}
-// move toolbar to the right side
-function toolbar_right() {
-    document.getElementById("color-container").style.left = "";
-    document.getElementById("tool-container").style.left = "";
-    document.getElementById("color-container").style.right = "0px";
-    document.getElementById("tool-container").style.right = "0px";
-}
-
-
-function tool1_inuse() {
-    drag_lock = false;
-    document.getElementById("pen").style.color = current_color;
-    document.getElementById("curve-pen").style.color = "gray";
-    document.getElementById("drag").style.color = "gray";
-    document.getElementById("eraser").style.color = "gray";
-    document.getElementById("roll").style.color = "gray";
-}
-
-function tool2_inuse() {
-    drag_lock = false;
-    document.getElementById("pen").style.color = "gray";
-    document.getElementById("curve-pen").style.color = current_color;
-    document.getElementById("drag").style.color = "gray";
-    document.getElementById("eraser").style.color = "gray";
-    document.getElementById("roll").style.color = "gray";
-}
-
-function tool3_inuse() {
-    drag_lock = false;
-    document.getElementById("pen").style.color = "gray";
-    document.getElementById("curve-pen").style.color = "gray";
-    document.getElementById("drag").style.color = "gray";
-    document.getElementById("eraser").style.color = "black";
-    document.getElementById("roll").style.color = "gray";
-}
-
-function tool4_inuse() {
-    drag_lock = false;
-    document.getElementById("pen").style.color = "gray";
-    document.getElementById("curve-pen").style.color = "gray";
-    document.getElementById("drag").style.color = "gray";
-    document.getElementById("eraser").style.color = "gray";
-    document.getElementById("roll").style.color = current_color;
-}
-
-
-function drag_canvas() {
-    drag_lock = true;
-    document.getElementById("pen").style.color = "gray";
-    document.getElementById("curve-pen").style.color = "gray";
-    document.getElementById("drag").style.color = "black";
-}
--- www/script/util.js
+++ www/script/util.js
@@ -17,3 +17,75 @@ function distance(x1, y1, x2, y2){
     return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
 }
 
+
+
+
+// move toolbar to the left side
+function toolbar_left() {
+    document.getElementById("color-container").style.left = "0px";
+    document.getElementById("tool-container").style.left = "0px";
+    document.getElementById("palette").style.left = "80px";
+    document.getElementById("color-container").style.right = "";
+    document.getElementById("tool-container").style.right = "";
+}
+// move toolbar to the right side
+function toolbar_right() {
+    document.getElementById("color-container").style.left = "";
+    document.getElementById("tool-container").style.left = "";
+    document.getElementById("palette").style.left = "-260px";
+    document.getElementById("color-container").style.right = "0px";
+    document.getElementById("tool-container").style.right = "0px";
+}
+
+
+
+
+
+TOOL_IDs = ["pen","curve-pen","drag","eraser","roll"];
+function highlight_tool(tool_id) {
+    for (i=0; i<TOOL_IDs.length; i++){
+        document.getElementById(TOOL_IDs[i]).style.color = "gray";
+    }
+    document.getElementById(tool_id).style.color = "black";
+}
+
+function tool1_inuse() {
+    drag_lock = false;
+    highlight_tool("pen");
+}
+
+function tool2_inuse() {
+    drag_lock = false;
+    highlight_tool("curve-pen");
+}
+
+function tool3_inuse() {
+    drag_lock = false;
+    highlight_tool("eraser");
+}
+
+function tool4_inuse() {
+    drag_lock = false;
+    highlight_tool("roll");
+}
+
+function drag_canvas() {
+    drag_lock = true;
+    highlight_tool("drag");
+}
+
+var palette_visible = false;
+function palette() {
+    drag_lock = false;
+    if (palette_visible){
+        document.getElementById("palette").style.visibility = "hidden";
+        palette_visible = false;
+    }
+    else{
+        document.getElementById("palette").style.visibility = "visible";
+        palette_visible = true;
+    }
+}
+
+
+
