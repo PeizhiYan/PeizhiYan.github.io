3b2d94b80056b133b0030fe6c54e2151271effa4
Peizhi Yan (Matthew) <yanpeizhi2008@yahoo.com>
2024-06-05 22:16:00 -0700

    3d viewer

---
+++ js_codes/real_3d_viewer/script.js
@@ -0,0 +1,212 @@
+
+// This is not a full .obj parser.
+// see http://paulbourke.net/dataformats/obj/
+function parseOBJ(text) {
+  // because indices are base 1 let's just fill in the 0th data
+  const objPositions = [[0, 0, 0]];
+  const objTexcoords = [[0, 0]];
+  const objNormals = [[0, 0, 0]];
+
+  // same order as `f` indices
+  const objVertexData = [
+    objPositions,
+    objTexcoords,
+    objNormals,
+  ];
+
+  // same order as `f` indices
+  let webglVertexData = [
+    [],   // positions
+    [],   // texcoords
+    [],   // normals
+  ];
+
+  function newGeometry() {
+    // If there is an existing geometry and it's
+    // not empty then start a new one.
+    if (geometry && geometry.data.position.length) {
+      geometry = undefined;
+    }
+    setGeometry();
+  }
+
+  function addVertex(vert) {
+    const ptn = vert.split('/');
+    ptn.forEach((objIndexStr, i) => {
+      if (!objIndexStr) {
+        return;
+      }
+      const objIndex = parseInt(objIndexStr);
+      const index = objIndex + (objIndex >= 0 ? 0 : objVertexData[i].length);
+      webglVertexData[i].push(...objVertexData[i][index]);
+    });
+  }
+
+  const keywords = {
+    v(parts) {
+      objPositions.push(parts.map(parseFloat));
+    },
+    vn(parts) {
+      objNormals.push(parts.map(parseFloat));
+    },
+    vt(parts) {
+      // should check for missing v and extra w?
+      objTexcoords.push(parts.map(parseFloat));
+    },
+    f(parts) {
+      const numTriangles = parts.length - 2;
+      for (let tri = 0; tri < numTriangles; ++tri) {
+        addVertex(parts[0]);
+        addVertex(parts[tri + 1]);
+        addVertex(parts[tri + 2]);
+      }
+    },
+  };
+
+  const keywordRE = /(\w*)(?: )*(.*)/;
+  const lines = text.split('\n');
+  for (let lineNo = 0; lineNo < lines.length; ++lineNo) {
+    const line = lines[lineNo].trim();
+    if (line === '' || line.startsWith('#')) {
+      continue;
+    }
+    const m = keywordRE.exec(line);
+    if (!m) {
+      continue;
+    }
+    const [, keyword, unparsedArgs] = m;
+    const parts = line.split(/\s+/).slice(1);
+    const handler = keywords[keyword];
+    if (!handler) {
+      console.warn('unhandled keyword:', keyword);  // eslint-disable-line no-console
+      continue;
+    }
+    handler(parts, unparsedArgs);
+  }
+
+  return {
+    position: webglVertexData[0],
+    texcoord: webglVertexData[1],
+    normal: webglVertexData[2],
+  };
+}
+
+
+
+
+
+
+async function main() {
+  // Get A WebGL context
+  const canvas = document.querySelector("#canvas");
+
+  const gl = canvas.getContext("webgl");
+  if (!gl) {
+    console.log('WebGL not supported.');
+    return;
+  }
+
+  // Set clear color to black, fully opaque
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+  const vs = `
+  attribute vec4 a_position;
+  attribute vec3 a_normal;
+
+  uniform mat4 u_projection;
+  uniform mat4 u_view;
+  uniform mat4 u_world;
+
+  varying vec3 v_normal;
+
+  void main() {
+    gl_Position = u_projection * u_view * u_world * a_position;
+    v_normal = mat3(u_world) * a_normal;
+  }
+  `;
+
+  const fs = `
+  precision mediump float;
+
+  varying vec3 v_normal;
+
+  uniform vec4 u_diffuse;
+  uniform vec3 u_lightDirection;
+
+  void main () {
+    vec3 normal = normalize(v_normal);
+    float fakeLight = dot(u_lightDirection, normal) * .5 + .5;
+    gl_FragColor = vec4(u_diffuse.rgb * fakeLight, u_diffuse.a);
+  }
+  `;
+
+  // Compile and link the shaders
+  const meshProgramInfo = webglUtils.createProgramInfo(gl, [vs, fs]);
+
+  obj_code_area = document.getElementById("obj");
+  const text = obj_code_area.value;
+  const data = parseOBJ(text);
+
+  // Create a buffer for each array
+  const bufferInfo = webglUtils.createBufferInfoFromArrays(gl, data);
+
+  const cameraTarget = [0, 0, 0];
+  const cameraPosition = [0, 0, 4];
+  const zNear = 0.1;
+  const zFar = 50;
+
+  function degToRad(deg) {
+    return deg * Math.PI / 180;
+  }
+
+  function render(time) {
+    time *= 0.0003;  // convert to seconds
+
+    webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    gl.enable(gl.DEPTH_TEST);
+    gl.enable(gl.CULL_FACE);
+
+    // Clear the canvas and the depth buffer
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    const fieldOfViewRadians = degToRad(60);
+    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    const projection = m4.perspective(fieldOfViewRadians, aspect, zNear, zFar);
+
+    const up = [0, 1, 0];
+    const camera = m4.lookAt(cameraPosition, cameraTarget, up);
+    const view = m4.inverse(camera);
+
+    const sharedUniforms = {
+      u_lightDirection: m4.normalize([-1, 3, 5]),
+      u_view: view,
+      u_projection: projection,
+    };
+
+    gl.useProgram(meshProgramInfo.program);
+
+    // Set uniforms
+    webglUtils.setUniforms(meshProgramInfo, sharedUniforms);
+
+    // Set buffers and attributes
+    webglUtils.setBuffersAndAttributes(gl, meshProgramInfo, bufferInfo);
+
+    // Set world matrix and draw
+    webglUtils.setUniforms(meshProgramInfo, {
+      u_world: m4.yRotation(time), // Rotate over time
+      u_diffuse: [1, 0.7, 0.5, 1],
+    });
+
+    // Draw the object
+    webglUtils.drawBufferInfo(gl, bufferInfo);
+
+    requestAnimationFrame(render);
+  }
+
+  requestAnimationFrame(render);
+}
+
+
+
+main();
